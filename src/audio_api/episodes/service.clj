d9a650bcad390a05bc05a371e0ee20f70fbcba47
Bruna Drago <bruna_drago@hotmail.com>
2026-07-07 19:36:28 -0300

    feat: add validation into service layer

--- src/audio_api/episodes/service.clj
+++ src/audio_api/episodes/service.clj
@@ -9,6 +9,29 @@
 (defn get-episode-by-id [id]
   (repository/find-by-id (UUID/fromString id)))
 
+(defn- validate-episode! [{:keys [id title duration-seconds published-at]}]
+  (when (str/blank? title)
+    (throw (ex-info "Title is required"
+                    {:type :validation-error
+                     :field :title})))
+
+  (when (or (nil? duration-seconds)
+            (not (number? duration-seconds))
+            (not (pos? duration-seconds)))
+    (throw (ex-info "Duration must be greater than zero"
+                    {:type :validation-error
+                     :field :duration-seconds})))
+
+  (when (str/blank? id)
+    (throw (ex-info "Id is required"
+                    {:type :validation-error
+                     :field :id})))
+
+  (when (str/blank? published-at)
+    (throw (ex-info "Published date is required"
+                    {:type :validation-error
+                     :field :published-at}))))
+
 (defn- prepare-episode [episode]
   (-> episode
       (update :id UUID/fromString)
@@ -16,4 +39,5 @@
 
 ;tem o ! porque tem efeito colateral, nao é pura
 (defn create-episode [episode]
+  (validate-episode! episode)
   (repository/create! (prepare-episode episode)))
